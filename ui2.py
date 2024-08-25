f427ac5848d028b84617b9b36eae3bee401d0f75
Glib116 <glebtomas99@gmail.com>
2024-08-25 13:01:59 +0300

    Add files via upload

---
+++ ui2.py
@@ -0,0 +1,22 @@
+import customtkinter
+import voiceBot
+from PIL import Image
+
+def button_callback():
+    print("button clicked")
+    textbox.delete("0.0", "end")
+    textbox.insert("0.0", voiceBot.main())
+
+app = customtkinter.CTk()
+app.geometry("400x150")
+
+button_image = customtkinter.CTKImage(Image.open("microphone.png"), size = (26,26))
+
+button = customtkinter.CTkButton(app, text="my button", command=button_callback)
+button.pack(padx=20, pady=20)
+
+textbox = customtkinter.CTkTextbox(app)
+textbox.insert("0.0","")
+textbox.pack(padx = 20, pady = 20)
+
+app.mainloop()
